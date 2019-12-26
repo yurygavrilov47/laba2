7499411aee99af8c8532613d38bfd94aebe61716
yurygavrilov47 <yurygavrilov47@gmail.com>
2019-12-26 20:39:34 +0300

    aaa

---
+++ sources/main.cpp
@@ -0,0 +1,9 @@
+//Copyright 2019 <yurygavrilov47>
+#include <Runner.hpp>
+int main() {
+    int a;
+    std::cout << "Введите количество уровней кэша = ";
+    std::cin >> a;
+    Runner Te(a);
+    return 0;
+}
